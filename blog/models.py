85f9c1a24de48b7600be6eeae8c896e922bb4752
Thorsten Roeske <thorsten@wearonize.com>
2024-09-11 13:20:43 +0000

    add: Meta Class and __str__ mehtod to Post and Comment Model

--- blog/models.py
+++ blog/models.py
@@ -5,26 +5,40 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    excerpt = models.TextField(blank=True)
-    author = models.ForeignKey(
+   title = models.CharField(max_length=200, unique=True)
+   slug = models.SlugField(max_length=200, unique=True)
+   excerpt = models.TextField(blank=True)
+   author = models.ForeignKey(
        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+   )
+   content = models.TextField()
+   created_on = models.DateTimeField(auto_now_add=True)
+   updated_on = models.DateTimeField(auto_now=True)
+   status = models.IntegerField(choices=STATUS, default=0)
+
+   class Meta:
+      ordering = ["-created_on"]
+
+   def __str__(self):
+      return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-       Post, on_delete=models.CASCADE, related_name="comments"
-    )
-    author = models.ForeignKey(
-       User, on_delete=models.CASCADE, related_name="commentor"
-    )
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    approved_on = models.DateTimeField(auto_now=True)
-    approved = models.BooleanField(default=False)
+   post = models.ForeignKey(
+      Post, on_delete=models.CASCADE, related_name="comments"
+   )
+   author = models.ForeignKey(
+      User, on_delete=models.CASCADE, related_name="comments_author"
+   )
+   body = models.TextField()
+   created_on = models.DateTimeField(auto_now_add=True)
+   approved_on = models.DateTimeField(auto_now=True)
+   approved = models.BooleanField(default=False)
+
+   class Meta:
+      ordering = ["-created_on"]
+
+   def __str__(self):
+      return f"Comment {self.body} by {self.author}"
+
+
