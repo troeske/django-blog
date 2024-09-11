730a2b8defb9e3a8177dc02f1bd5321d70cfe265
Thorsten Roeske <thorsten@wearonize.com>
2024-09-11 12:15:32 +0000

    add: updated_on and excerpt to post table

--- blog/models.py
+++ blog/models.py
@@ -7,11 +7,13 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    excerpt = models.TextField(blank=True)
     author = models.ForeignKey(
        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
 
