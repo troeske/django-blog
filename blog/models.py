4398bdfdf9001e3001f0c9d26b54de077ad7c9fd
Thorsten Roeske <thorsten@wearonize.com>
2024-09-11 12:57:25 +0000

    add: comment model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,14 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+       Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+       User, on_delete=models.CASCADE, related_name="commentor"
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanFieldField(default=False)
