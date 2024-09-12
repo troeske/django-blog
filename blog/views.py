0bf5ca9f14d5448eaed5d0118c997f924f7793d1
Thorsten Roeske <thorsten@wearonize.com>
2024-09-12 17:05:33 +0000

    add: comment section on post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -24,9 +24,13 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
 
-    context = {"post": post,} 
-               # "coder": "Thorsten Roeske"}
+    context = {"post": post,
+               "comments": comments,
+               "comment_count": comment_count,
+               }
                
 
     return render(
