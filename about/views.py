b53a4ce9c76c89d7a16b06940422736bc9e51884
Thorsten Roeske <thorsten@wearonize.com>
2024-09-13 09:31:48 +0000

    add: save collaboration request in to db

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About, CollaborateRequest
 from .forms import CollaborateForm
 
@@ -9,11 +10,20 @@ def about_me(request):
     """
     about = About.objects.all().order_by('-updated_on').first()
 
-    """ return render(
-        request,
-        "about/about.html",
-        {"about": about},
-    ) """
+    if request.method == "POST":
+        print("Received a POST request")
+        collaboration_form = CollaborateForm(data=request.POST)
+        if collaboration_form.is_valid():
+            collaboration_form.save()
+            """ collaboration.author = request.user
+            collaboration.name = 
+            collaboration.email =
+            collaboration.message =
+            collaboration.save() """
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavor to respond within 2 working days.'
+                )
 
     collaboration_form = CollaborateForm()
 
