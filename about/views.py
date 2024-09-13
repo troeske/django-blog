4bb7ccba710fceffa47d954f93e23ee6e3f907e4
Thorsten Roeske <thorsten@wearonize.com>
2024-09-13 09:07:12 +0000

    add: collaboaration request form in about app

--- about/views.py
+++ about/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
-from .models import About
+from .models import About, CollaborateRequest
+from .forms import CollaborateForm
+
 # Create your views here.
-
-
 def about_me(request):
     """
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
 
-    return render(
+    """ return render(
         request,
         "about/about.html",
         {"about": about},
+    ) """
+
+    collaboration_form = CollaborateForm()
+
+    context = {"about": about,
+               "collaboration_form": collaboration_form
+               }
+            
+    return render(
+         request,
+        "about/about.html",
+        context,
     )
